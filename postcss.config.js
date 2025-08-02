aba3ba0d32c600fb14f3a55948d373b0dcb3502b
PARTHASARATHYPALANIALAGU <sarathyalagu@gmail.com>
2025-08-02 13:55:18 +0530

    package modify

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+// postcss.config.js
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss/nesting'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    process.env.NODE_ENV === 'production' && require('cssnano')
+  ].filter(Boolean)
 }
